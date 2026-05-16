7147b5fcb59204cb82c1b51c7984a4f4de8e46d6
Robin Bolinsson <robin@komadori.se>
2026-05-16 14:00:42 +0200

    Bump fork version to 1.5.3

--- _meta.lua
+++ _meta.lua
@@ -4,6 +4,6 @@ return {
     name        = "simpleui",
     fullname    = "Simple UI",
     description = [[A simple UI for KOReader]],
-    version     = "1.5.2",
+    version     = "1.5.3",
     author      = "Doctor Hetfield",
 }
