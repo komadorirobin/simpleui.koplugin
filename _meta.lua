99e6ddb7a3d3089d036eb60899e9b0a3ee457193
Robin Bolinsson <robin@komadori.se>
2026-07-10 13:35:52 +0200

    Bump SimpleUI to 2.1.2

--- _meta.lua
+++ _meta.lua
@@ -4,6 +4,6 @@ return {
     name        = "simpleui",
     fullname    = "Simple UI",
     description = [[A simple UI for KOReader]],
-    version     = "2.1.1",
+    version     = "2.1.2",
     author      = "Doctor Hetfield",
 }
