5659c772c35fd38a1e72cd09e52370ef70f4d238
Robin Bolinsson <robin@komadori.se>
2026-08-29 00:21:46 +0200

    Release SimpleUI 2.6.4

--- _meta.lua
+++ _meta.lua
@@ -4,6 +4,6 @@ return {
     name        = "simpleui",
     fullname    = _("Simple UI"),
     description = _([[A simple UI for KOReader]]),
-    version     = "2.6.3",
+    version     = "2.6.4",
     author      = "Doctor Hetfield",
 }
